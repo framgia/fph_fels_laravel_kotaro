2b847a4303f99b79f1f9cddc4220de1ba5a9b0c4
Koutarou Takayama <lilik@KotaronoMacBook-puro.local>
2019-02-06 20:52:31 +0800

    add option 'nullable' to 'avatar_url' table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('avatar_url');
+            $table->string('avatar_url')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('admin')->default(false);
